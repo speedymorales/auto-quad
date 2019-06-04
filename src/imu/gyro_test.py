843d653c6ccd493ad39fbdcfff6e4468842b2e1e
Jose Morales <speedymorales5@gmail.com>
2019-06-04 15:21:29 -0400

    Gyro Z data capture fix. Condensed code for acquiring dx, dy, dz.

--- src/imu/gyro_test.py
+++ src/imu/gyro_test.py
@@ -28,9 +28,9 @@ INT1_THS_ZL = 0x37
 INT_DURATION = 0x38
 
 OUT_TEMP = 0x26
-OUT_X = 0x28
-OUT_Y = 0x2A
-OUT_Z = 0x2D
+OUT_X_16 = 0x28
+OUT_Y_16 = 0x2A
+OUT_Z_16 = 0x2C
 
 def readWord(addr, cmd):
   # dSet = bus.read_i2c_block_data(addr, cmd)
@@ -52,10 +52,10 @@ def signedWord(word):
     return word
 
 def init():
-  bus.write_byte_data(DEVICE_ADDRESS, CTRL_REG1, 0x0F) # 95 Hz, 12.5 cut-off, Powerdown Off, XYZ Enabled
+  bus.write_byte_data(DEVICE_ADDRESS, CTRL_REG1, 0x0F) # Powerdown High (off), XYZ Enabled
   bus.write_byte_data(DEVICE_ADDRESS, CTRL_REG2, 0x00) # High pass mode: normal, Cut-Off Freq: 7.2 Hz
   bus.write_byte_data(DEVICE_ADDRESS, CTRL_REG3, 0x00)
-  bus.write_byte_data(DEVICE_ADDRESS, CTRL_REG4, 0x10) # Block data update: complete, BLE: LSB, Scale: 500 dps
+  bus.write_byte_data(DEVICE_ADDRESS, CTRL_REG4, 0x10) # Block data update: complete, BLE: LSB, Scale: 00 - 250, 01 - 500, 11 - 2000 dps
   bus.write_byte_data(DEVICE_ADDRESS, CTRL_REG5, 0x00) # FIFO disabled, High Pass Disabled
 
 init()
@@ -63,23 +63,17 @@ init()
 while True:
   # sleep(0.02)
   sleep(1)
-  
-  dX = signedWord(readWord(DEVICE_ADDRESS, OUT_X))
-  dY = signedWord(readWord(DEVICE_ADDRESS, OUT_Y))
-  dZ = signedWord(readWord(DEVICE_ADDRESS, OUT_Z))
 
-  # dSet0 = bus.read_byte_data(DEVICE_ADDRESS, OUT_X)
-  # dSet1 = bus.read_byte_data(DEVICE_ADDRESS, OUT_X + 1)
+  status = bus.read_byte_data(DEVICE_ADDRESS, STATUS_REG)
+  # print("{0:b}".format(status))
 
-  # print(dSet0)
-  # print(dSet1)
+  while status & 0x07 != 0x07:
+    # sleep(0.005)
+    status = bus.read_byte_data(DEVICE_ADDRESS, STATUS_REG)
+  dX = signedWord(readWord(DEVICE_ADDRESS, OUT_X_16))
+  dY = signedWord(readWord(DEVICE_ADDRESS, OUT_Y_16))
+  dZ = signedWord(readWord(DEVICE_ADDRESS, OUT_Z_16))
 
-  # word = dSet1 << 8 | dSet0
-  # print(word)
-  # print(signedWord(word))
-
-  print('X: ' + str(dX))
-  print('Y: ' + str(dY))
-  print('Z: ' + str(dZ))
+  print('X: ' + str(dX) + ' Y: ' + str(dY) + ' Z: ' + str(dZ))
   print("")
   
