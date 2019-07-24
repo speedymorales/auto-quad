97eff9363e52222b5484968910b52a0f3d212505
Morales <morluisj@UFL.EDU>
2019-07-24 20:44:03 +0100

    Working networking protocol.

--- src/network/client.py
+++ src/network/client.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3           # This is client.py file
 
 import socket
+import struct
+import sys
 
 # create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # tst = 5.123
 # print(type(tst))
-print(tst)
+# print(tst)
 
 # host = socket.gethostname()
 host = 'proxy.jlmo.info'
+host = 'localhost'
 
 port = 9999
 
@@ -18,7 +21,27 @@ port = 9999
 s.connect((host, port))
 
 # Receive no more than 1024 bytes
-msg = s.recv(1024)
+msg = s.recv(6)
+print([ "0x%02x" % b for b in msg ])
+# sys.stdout.flush()
 
-s.close()
-print (msg.decode('ascii'))
+header = msg[0:4].decode()
+print(header)
+
+if header == 'DATA':
+  size = struct.unpack('H', msg[4:6])[0]
+  print(size)
+  msg = s.recv(size)
+  print([ "0x%02x" % b for b in msg ])
+
+  for i in range(0, int(size/8)):
+    print(struct.unpack('d', msg[i*8: i*8+8])[0])
+
+  # print([ "0x%02x" % b for b in msg[0:8] ])
+  # print(struct.unpack("d", msg[0:8])[0])
+
+  # print([ "0x%02x" % b for b in msg[8:16] ])
+  # print(struct.unpack("d", msg[8:16])[0])
+
+  s.close()
+  # print (msg.decode('ascii'))
--- src/network/server.py
+++ src/network/server.py
@@ -2,6 +2,7 @@
 import socket
 import sys
 import netifaces as ni
+import struct
 
 # create a socket object
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,7 +11,8 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # host = '127.0.0.1' #socket.gethostname()
 
 # ni.ifaddresses('wlan0')
-host = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+# host = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+host = 'localhost'
 
 port = 9999
 
@@ -18,15 +20,32 @@ port = 9999
 serversocket.bind((host, port))
 
 # queue up to 5 requests
-serversocket.listen(5)
+# serversocket.listen(5)
+serversocket.listen()
 
 while True:
    # establish a connection
-   clientsocket,addr = serversocket.accept()
+   clientsocket, addr = serversocket.accept()
 
    print("Got a connection from %s" % str(addr))
-   sys.stdout.flush()
-    
+   
    msg = 'Thank you for connecting'+ "\r\n"
-   clientsocket.send(msg.encode('ascii'))
+   msg = bytearray()
+   msg.extend('DATA'.encode())
+
+   value = [float(5.10001), float(600.0), float(123.45), float(100), float(0), float(1.1), float(24.25), float(0.001), float(1.112)]
+
+   datagram = b''
+   for f in value:
+      datagram = datagram + struct.pack("d", f)
+   
+   print([ "0x%02x" % b for b in datagram ])
+   print(len(datagram))
+
+   msg = msg + struct.pack('H', len(datagram))
+   msg = msg + datagram
+
+   clientsocket.send(msg)
+
+   sys.stdout.flush()
    clientsocket.close()
